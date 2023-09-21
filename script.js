40e2554ca42cdf0b3558b164eb218b7e325c3255
eduardo segato <eduardosegato@outlook.com>
2023-09-21 18:20:31 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleMode() {
+  const html = document.documentElement 
+  html.classList.toggle('light') // comando que substitui o "if else" abaixo, se tiver o light ele tira, se não tiver ele coloca
+  
+  // pegar a tag img 
+  const img = document.querySelector("#profile img")
+  
+  //substituir a imagem
+  if(html.classList.contains('light')) {
+    // se tiver o light mode, adicionar imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // se tiver sem o light mode, manter imagem original
+    img.setAttribute("src", "./assets/Eu.jpeg")
+  }
+
+
+  /*
+  if(html.classList.contains('light')) {
+    html.classList.remove('light')
+  } else {
+    html.classList.add('light')
+  }
+  */
+
+
+
+}
